7386a5e916e26f29b577072106ea00718d43a968
morockz26 <mohammedosumah@gmail.com>
2025-02-11 04:40:52 -0500

    added tests

--- test/cdk-test.test.ts
+++ test/cdk-test.test.ts
@@ -1,17 +1,73 @@
-// import * as cdk from 'aws-cdk-lib';
-// import { Template } from 'aws-cdk-lib/assertions';
-// import * as CdkTest from '../lib/cdk-test-stack';
+import * as cdk from 'aws-cdk-lib';
+import { Template } from 'aws-cdk-lib/assertions';
+import * as CdkTest from '../lib/cdk-test-stack';
 
-// example test. To run these tests, uncomment this file along with the
-// example resource in lib/cdk-test-stack.ts
-test('SQS Queue Created', () => {
-//   const app = new cdk.App();
-//     // WHEN
-//   const stack = new CdkTest.CdkTestStack(app, 'MyTestStack');
-//     // THEN
-//   const template = Template.fromStack(stack);
+describe('CdkTestStack', () => {
+  it('Level 1 Bucket Created with Versioning Enabled', () => {
+    const app = new cdk.App();
+    const stack = new CdkTest.CdkTestStack(app, 'MyTestStack');
+    const template = Template.fromStack(stack);
 
-//   template.hasResourceProperties('AWS::SQS::Queue', {
-//     VisibilityTimeout: 300
-//   });
+    template.hasResourceProperties('AWS::S3::Bucket', {
+      VersioningConfiguration: {
+        Status: "Enabled"
+      }
+    });
+  });
+
+  it('Level 2 Bucket Created with Proper Properties', () => {
+    const app = new cdk.App();
+    const stack = new CdkTest.CdkTestStack(app, 'MyTestStack');
+    const template = Template.fromStack(stack);
+  
+    template.hasResourceProperties('AWS::S3::Bucket', {
+      BucketName: {
+        "Fn::Join": [
+          "", [
+            "myfirstlevel2constructbucket-",
+            { "Ref": "AWS::AccountId" },
+            "-",
+            { "Ref": "AWS::Region" }
+          ]
+        ]
+      },
+      CorsConfiguration: {
+        CorsRules: [
+          {
+            AllowedMethods: [
+              "GET", "HEAD", "POST", "PUT", "DELETE"
+            ],
+            AllowedOrigins: ["*"],
+            AllowedHeaders: ["*"]
+          }
+        ]
+      }
+    });
+  });
+
+  it('SQS Queue Created with Correct Name', () => {
+    const app = new cdk.App();
+    const stack = new CdkTest.CdkTestStack(app, 'MyTestStack');
+    const template = Template.fromStack(stack);
+
+    template.hasResourceProperties('AWS::SQS::Queue', {
+      QueueName: "MyQueue"
+    });
+  });
+
+  it('S3 Bucket is Configured with Notification to SQS Queue', () => {
+    const app = new cdk.App();
+    const stack = new CdkTest.CdkTestStack(app, 'MyTestStack');
+    const template = Template.fromStack(stack);
+  
+    template.hasResourceProperties('Custom::S3BucketNotifications', {
+      NotificationConfiguration: {
+        QueueConfigurations: [
+          {
+            Events: ["s3:ObjectCreated:*"],
+          },
+        ]
+      }
+    });
+  });  
 });
